2b438f336d960d337d587321c27f3c799438f9e7
Joseph Tsindos <jtsindos@gmail.com>
2019-12-19 14:13:52 +1100

    Some work using material-ui grid.

--- src/BodyMenuItem/BodyMenuItem.js
+++ src/BodyMenuItem/BodyMenuItem.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
 
-export default () =>
-  <div class='row around'>
-    <div class='col-xs-6'>
-      <div class='box'>First </div>
+import laptop from '../../img/laptop.jpg'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }
+}))
+
+export default function AutoGrid () {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Grid container justify='center' spacing={3}>
+            <Grid item>
+              <p>
+                Explore Cyber lnalnflanlfnalfnlanfl
+              </p>
+            </Grid>
+            <Grid item>
+              <img src={laptop} style={{ maxWidth: '600px' }} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justify='center' spacing={3}>
+            {[0, 1, 2].map(value => (
+              <Grid key={value} item xs>
+                <Paper className={classes.paper} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
     </div>
-    <div class='col-lg-6'> Second </div>
-  </div>
+  )
+}
