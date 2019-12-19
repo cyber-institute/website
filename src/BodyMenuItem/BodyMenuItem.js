71a57175136fee5e56b36bbbfe72dd7a8c2b4c06
Joseph Tsindos <jtsindos@gmail.com>
2019-12-19 15:30:58 +1100

    Work on BodyMenuItem.

--- src/BodyMenuItem/BodyMenuItem.js
+++ src/BodyMenuItem/BodyMenuItem.js
@@ -1,62 +1,62 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid'
+
+import Button from '@material-ui/core/Button'
+import ArrowRightIcon from '@material-ui/icons/ArrowRight'
+
+import './style.css'
 
 import laptop from '../../img/laptop.jpg'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary
-  }
-}))
-
-export default function AutoGrid () {
-  const classes = useStyles()
+export default function BodyMenuItem () {
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Grid container justify='center' spacing={3}>
-            <Grid item>
-              <p>
-                Explore Cyber lnalnflanlfnalfnlanfl
-              </p>
-            </Grid>
-            <Grid item>
-              <img src={laptop} style={{ maxWidth: '600px' }} />
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid container className={classes.root} spacing={2}>
-        <Grid item xs={12}>
-          <Grid container justify='center' spacing={3}>
-            {[0, 1, 2].map(value => (
-              <Grid key={value} item xs>
-                <Paper className={classes.paper} />
-              </Grid>
-            ))}
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-      </Grid>
+    <div style={{ marginTop: '20rem' }}>
+      <div class='row'>
+        <div class='col-xs-offset-2 col-xs-4'>
+          <div
+            style={{
+              marginBottom: '1em',
+              color: 'rgb(50, 50, 50)',
+              letterSpacing: '.05em',
+              fontSize: '.72rem'
+            }}
+            class='Medium'
+          >
+            01 About
+          </div>
+          <div
+            class='largerHeading Bold' style={{
+              paddingTop: '0.4em',
+              borderTop: '1px solid currentColor',
+              marginBottom: '2.1rem'
+            }}
+          >
+            Explore Cyber
+          </div>
+          <div class='Light row'>
+            <div class='col-xs-9 Light' style={{ lineHeight: '1.87' }}>
+              Mission-driven. Exploratory. Interdisciplinary and inclusive in nature. Trusted adviser to decision makers.
+            </div>
+          </div>
+          <Button
+            variant='outlined' color='#523178' style={{
+              margin: '3rem 0',
+              color: '#523178',
+              borderColor: '#523178',
+              textTransform: 'none',
+              fontFamily: 'Helvetica Neue LT Std 65 Medium',
+              fontSize: '14px'
+            }}
+          >
+            About Cyber Institute
+            <ArrowRightIcon />
+          </Button>
+        </div>
+        <div class='col-xs-4'>
+          <img src={laptop} style={{ maxWidth: '100%' }} />
+        </div>
+        <div class='col-xs-2' />
+      </div>
     </div>
   )
 }
