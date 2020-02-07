7fa0ecab1eb96a555b99e89ff5089e81d1652c09
Joseph Tsindos <jtsindos@gmail.com>
2020-02-07 12:23:26 +1100

    Add Cyber Mastery SlideToggleContent.

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Banner from '../common/Banner'
+import Slider from '../common/Slider'
 
 import banner from './banner.jpg'
 
@@ -46,6 +47,17 @@ We call it building your Cyber Mastery.
           </p>
         </div>
       </div>
+      <div
+        className='row' style={{
+          marginTop: '8rem'
+        }}
+      >
+        <div className='col-md-offset-1 col-md-10'>
+          <Slider name='Cyber Mastery'>
+            <CyberMasteryContent />
+          </Slider>
+        </div>
+      </div>
     </div>
   </>
 
@@ -60,3 +72,61 @@ const Heading = ({ children }) =>
   >
     {children}
   </h2>
+
+const CyberMasteryContent = () =>
+  <>
+    <div className='row'>
+      <div className='col-md-offset-1 col-md-5'>
+        <div>
+      Course Length: 1 year full-time study
+        </div>
+        <div>
+        Enrolment information coming in near future
+        </div>
+        <p>Its core principles are:</p>
+        {
+          Object.entries(content).map(([title, content], index) => {
+            if (index % 2 === 0) {
+              return (
+                <div>
+                  <h3>{index + 1}. {title}</h3>
+                  <p>{content}</p>
+                </div>
+              )
+            }
+          })
+        }
+      </div>
+      <div className='col-md-5'>
+        <div>
+      Commencing cohort: 2021 (with some modules available from mid-2020)
+        </div>
+        <div>
+        Includes: Micro modules & Three capstone projects
+        </div>
+        {
+          Object.entries(content).map(([title, content], index) => {
+            if (index % 2 !== 0) {
+              return (
+                <div>
+                  <h3>{index + 1}. {title}</h3>
+                  <p>{content}</p>
+                </div>
+              )
+            }
+          })
+        }
+      </div>
+    </div>
+  </>
+
+const content = {
+  Interdisiplinarity: 'The problems presented by cyber are difficult precisely because they do not fit neatly into traditional ways of thinking or managing problems, solutions, opportunities and risks',
+  'Experiential learning': 'We believe the best way to understand core insights and concepts is to apply them. That it is why we’ll be working closely with people across the University to offer you real-time experience as appropriate, as well using a range of tools to help your learning',
+
+  'Conceptual thinking': 'We could teach content and description, but that’s inflexible and would age quickly. It is much better to help you to understand and develop the conceptual frameworks you can adjust and apply to meet the unexpected',
+
+  'Individuals and teams': 'While it’s important that you learn as an individual, cyber is inherently a team effort. Through our program you’ll understand better your own strengths and weaknesses, and how to work well with a diverse range of stakeholders and skill sets; and,',
+
+  Responsiveness: 'We want to ensure that we cover what we consider to be the core craft of cyber and associated topics and offer it in a way that recognises the demanding schedules and needs of busy professionals. Moreover, we know that it is the network of friends you will build as you test yourselves together that will stay with you and help you in future roles. We see this as a core outcome of the program.'
+}
---
+++ src/pages/common/Slider.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+import SlideToggleContent from './SlideToggleContent'
+
+export default ({ name, children }) => {
+  const [isVisible, setIsVisible] = useState(true)
+  return (
+    <div style={{
+      marginBottom: '1rem'
+    }}
+    >
+      <button
+        type='button'
+        style={{
+          border: 'none',
+          padding: 0
+        }}
+        onClick={() => setIsVisible(!isVisible)}
+      >
+        <Name {...{ name }} />
+        <Icons {...{ isVisible }} />
+      </button>
+      <SlideToggleContent {...{ isVisible }}>
+        <div style={{
+          margin: '1rem 0'
+        }}
+        >
+          <div>
+            {children}
+          </div>
+        </div>
+      </SlideToggleContent>
+    </div>
+  )
+}
+
+const Icons = ({ isVisible }) =>
+  isVisible
+    ? (
+      <ExpandLessIcon
+        style={{
+          verticalAlign: 'middle',
+          opacity: '0.8',
+          color: '#523178',
+          fontSize: '60px'
+        }}
+      />
+    )
+    : (
+      <ExpandMoreIcon
+        style={{
+          verticalAlign: 'middle',
+          opacity: '0.9',
+          color: '#523178',
+          fontSize: '60px'
+        }}
+      />
+    )
+
+const Name = ({ name }) =>
+  <span
+    className='Bold' style={{
+      verticalAlign: 'middle',
+      fontSize: '40px',
+      letterSpacing: '6px',
+      color: 'rgb(82, 49, 120)'
+    }}
+  >
+    {name}
+  </span>
