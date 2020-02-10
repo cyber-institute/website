aeb77a4f1f3adbcf3d4e1444e7d3440ea38f71be
Joseph Tsindos <jtsindos@gmail.com>
2020-02-10 17:02:43 +1100

    Complete PartnerWithUs.js, fully responsive.

--- src/pages/Services/PartnerWithUs.js
+++ src/pages/Services/PartnerWithUs.js
@@ -3,17 +3,21 @@ import React from 'react'
 import backgroundArt from './backgroundArt.png'
 
 export default () =>
-  <div style={{
-    position: 'relative',
-    height: '100vh'
-  }}
+  <div
+    className='container-fluid' style={{
+      position: 'relative'
+    }}
   >
     <div style={{
       background: '0 0',
       overflow: 'hidden',
-      position: 'relative',
+      position: 'absolute',
+      top: '0',
+      right: '0',
+      bottom: '0',
+      left: '0',
       width: '100%',
-      zIndex: '0',
+      zIndex: -1,
       height: '100%',
       backgroundColor: 'rgb(82, 49, 120)'
     }}
@@ -22,7 +26,7 @@ export default () =>
         className='bannerImg'
         style={{
           height: '100%',
-          position: 'relative',
+          position: 'absolute',
           display: 'block',
           marginRight: 'auto',
           width: '100%',
@@ -34,89 +38,81 @@ export default () =>
         }}
       />
     </div>
-    <div
-      className='container-fluid' style={{
-        position: 'absolute',
-        top: '0',
-        right: '0',
-        bottom: '0',
-        left: '0'
-      }}
-    >
-      <div className='row'>
-        <div
-          className='col-md-offset-3 col-md-6' style={{
-            textAlign: 'center'
+    <div className='row'>
+      <div
+        className='col-md-offset-3 col-md-6' style={{
+          textAlign: 'center'
+        }}
+      >
+        <h2
+          className='Light' style={{
+            fontSize: '49px',
+            letterSpacing: '6px',
+            color: '#fff',
+            margin: '3rem 0 3rem'
           }}
         >
-          <h2
-            className='Light' style={{
-              fontSize: '49px',
-              letterSpacing: '6px',
-              color: '#fff'
+          <div
+            className='displayNone-sm' style={{
+              position: 'absolute',
+              left: 0,
+              width: '30%'
             }}
           >
             <div
-              className='displayNone-sm' style={{
-                position: 'absolute',
-                left: 0,
-                width: '30%'
+              style={{
+                position: 'relative',
+                top: '1em',
+                border: '1px solid #fff',
+                opacity: '0.5'
               }}
-            >
-              <div
-                style={{
-                  position: 'relative',
-                  top: '1em',
-                  border: '1px solid #fff',
-                  opacity: '0.5'
-                }}
-              />
-            </div>
+            />
+          </div>
+          <div
+            className='displayNone-sm' style={{
+              position: 'absolute',
+              right: 0,
+              width: '30%'
+            }}
+          >
             <div
-              className='displayNone-sm' style={{
-                position: 'absolute',
-                right: 0,
-                width: '30%'
+              style={{
+                position: 'relative',
+                top: '1em',
+                border: '1px solid #fff',
+                opacity: '0.5'
               }}
-            >
-              <div
-                style={{
-                  position: 'relative',
-                  top: '1em',
-                  border: '1px solid #fff',
-                  opacity: '0.5'
-                }}
-              />
-            </div>
+            />
+          </div>
           Why not partner<br /> with us?
-          </h2>
-          <p
-            style={{
-              fontSize: '20px',
-              color: '#fff'
-            }}
-          >
+        </h2>
+        <p
+          style={{
+            fontSize: '20px',
+            color: '#fff'
+          }}
+        >
           If your organisation wants to be involved, contact us.  There are many opportunities to both contribute to, as well as benefit from, our education program.
-            <br />
+          <br />
 We are partnering with a diverse set of organisations and celebrate the diversity of our people and skills. We believe the best outcomes arise from collaboration between researchers and practitioners with a range of experiences, skills and insights.
-          </p>
-          <p
-            style={{
-              fontSize: '16px',
-              color: '#fff'
-            }}
-          >
-            <br />
-            <br />
+        </p>
+        <p
+          style={{
+            fontSize: '16px',
+            color: '#fff',
+            marginBottom: '3rem'
+          }}
+        >
+          <br />
+          <br />
 There are also opportunities to collaborate on content design. That gives the experience we offer immediacy and relevance
-            <br />
-            <br />
+          <br />
+          <br />
 In addition, you or your staff may wish to be part of our community of mentors. We see cyber mentors as intrinsic to our education program. They also help build capability through insights and opportunities, they offer a community of trust and support to our graduates.
-            <br />
-            <br />
+          <br />
+          <br />
 Some partners also sponsor students, however you may wish only for your own staff to gain the benefits of undertaking the program.  Because of our development and delivery mechanism, we have the flexibility to develop modules as Executive Education tailored to your organisational needs, or to address specific topics as identified by you.
-          </p>
-        </div>
+        </p>
       </div>
     </div>
   </div>
