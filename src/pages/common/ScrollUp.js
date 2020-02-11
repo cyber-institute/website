ece2ecd7e7e29f4abf8ce5c5410943db92119b62
Joseph Tsindos <jtsindos@gmail.com>
2020-02-11 16:55:13 +1100

    Make ScrollUp component.

---
+++ src/pages/common/ScrollUp.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+import * as Scroll from 'react-scroll'
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+
+export default class ScrollUp extends Component {
+  componentDidMount () {
+    scrollSpy.update()
+  }
+
+  handleScrollToTop () {
+    scroll.scrollToTop()
+  }
+
+  render () {
+    return (
+      <>
+        <button
+          title='Back to top'
+          style={{
+            opacity: '0.8',
+            backgroundColor: 'rgb(82, 49, 120)',
+            width: '40px',
+            height: '40px',
+            position: 'fixed',
+            bottom: '10px',
+            right: '10px',
+            borderRadius: '5px',
+            border: 'none',
+            zIndex: '1',
+            cursor: 'pointer'
+          }}
+          onClick={this.handleScrollToTop}
+        >
+          <ExpandLessIcon style={{
+            color: '#fff'
+          }}
+          />
+        </button>
+        {this.props.children}
+      </>
+    )
+  }
+}
