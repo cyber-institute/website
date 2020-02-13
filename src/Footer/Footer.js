aebaed8318e0f660ec7bfd8dce9cc5bd603d3754
kaitlin-wickham <60117519+kaitlin-wickham@users.noreply.github.com>
2020-02-13 16:27:18 +1100

    Update Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 import './style.css'
-import logo from '.../img/logo.png'
+import logo from '../../img/logo.png'
 
 class Footer extends Component {
   render () {
