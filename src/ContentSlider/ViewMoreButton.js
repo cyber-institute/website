fd6ab106e0d3bbfccb02698b0e641a89dbc10067
kaitlin-wickham <60117519+kaitlin-wickham@users.noreply.github.com>
2020-02-14 17:27:14 +1100

    Update ViewMoreButton.js

--- src/ContentSlider/ViewMoreButton.js
+++ src/ContentSlider/ViewMoreButton.js
@@ -18,6 +18,8 @@ export default ({ clickButton, isVisible, alignRight }) =>
         color: '#000',
         backgroundColor: '#fff',
         fontSize: '18px',
+        'font-family': 'Roboto, sans-serif',
+        'font-weight': '300',
         letterSpacing: '0.25px',
         border: 'none'
       }}
