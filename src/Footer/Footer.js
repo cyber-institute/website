f4eecde0a66d5c541295e5c1c556602cce79c3e2
Joseph Tsindos <jtsindos@gmail.com>
2020-02-14 17:33:48 +1100

    Footer now compressable.

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -9,25 +9,37 @@ class Footer extends Component {
       <>
         <div style={{ 'margin-top': '200px' }}>
           <div className='footerContainer' />
-
-          <div className='textContainer'>
-            <div className='footerText'>
-              <p className='fontSize16px-xs'>
-        +61 2 6125 7802
-              </p>
-              <p>
-                <a className='fontSize16px-xs' href='mailto:cyber@anu.edu.au' style={{ color: '#D2ACF7', 'text-decoration': 'underline' }}>cyber@anu.edu.au</a>
-              </p>
-              <p className='fontSize16px-xs'>
-        Level 2, 115 North Rd, Acton  ACT 2601
-              </p>
-              <p>
-                <a className='fontSize16px-xs' href='https://twitter.com/anucyber?lang=en' style={{ color: '#D2ACF7', 'text-decoration': 'underline' }}>twitter@AnuCyber</a>
-              </p>
+          <div className='textContainer row' style={{
+            margin: 0
+          }}>
+            <div className='col-xs-6' style={{
+              textAlign: 'right'
+            }}>
+              <div>
+                <img src={logo} className='logo' alt="The Cyber Institute's Logo in White" style={{
+                  width: '100%',
+                  maxWidth: '400px',
+                }}/>
+              </div>
             </div>
-
-            <img src={logo} className='logo' alt="The Cyber Institute's Logo in White" />
-
+            <div className='col-xs-6'>
+              <div className='footerText'>
+                <p className='fontSize16px-xs' style={{
+                  paddingTop: 0
+                }}>
+        +61 2 6125 7802
+                </p>
+                <p>
+                  <a className='fontSize16px-xs' href='mailto:cyber@anu.edu.au' style={{ color: '#D2ACF7', 'text-decoration': 'underline' }}>cyber@anu.edu.au</a>
+                </p>
+                <p className='fontSize16px-xs'>
+        Level 2, 115 North Rd, Acton  ACT 2601
+                </p>
+                <p>
+                  <a className='fontSize16px-xs' href='https://twitter.com/anucyber?lang=en' style={{ color: '#D2ACF7', 'text-decoration': 'underline' }}>twitter@AnuCyber</a>
+                </p>
+              </div>
+            </div>
           </div>
 
           <div className='purpleStrip fontSize16px-xs'>
