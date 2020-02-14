c9d4b3dea795d2527f5fe329a3bd6305b1ab3953
kaitlin-wickham <60117519+kaitlin-wickham@users.noreply.github.com>
2020-02-14 11:44:48 +1100

    Update Operations.js

--- src/pages/Services/Operations.js
+++ src/pages/Services/Operations.js
@@ -35,13 +35,13 @@ export default ({ image1, image2, image3 }) =>
         </div>
         <div className='col-md-6'>
           <p>
-        Hands-on education and real-life operations training are integral to the Institute’s programs, and one of its key differentiators. The Cyber Edge will integrate the cybersecurity operations function of the ANU into the academic program. We consider that this represents a step-change in research, education and service provision.
+        <span style={{'font-weight':'700'}}>Hands-on education and real-life operations training</span> are integral to the Institute’s programs, and one of its key differentiators. The Cyber Edge will integrate the cybersecurity operations function of the ANU into the academic program. We consider that this represents a step-change in research, education and service provision.
             <br />
             <br />
 Together with an online presence, the Institute’s Cyber Edge will provide the opportunity to test ideas, and communicate and visualise cyber threats, mitigations and activities.
             <br />
             <br />
-Access to a real-time operational environment provides ideal training and research conditions. The Cyber Edge will cover a range of bespoke and highly specialised technologies, operated by a diverse and ever-changing mix of staff, students, and partners on a distributed network within an academic institution.
+<span style={{'font-weight':'700', color:'#523178'}}>Access to a real-time operational environment provides ideal training and research conditions</span>. The Cyber Edge will cover a range of bespoke and highly specialised technologies, operated by a diverse and ever-changing mix of staff, students, and partners on a distributed network within an academic institution.
             <br />
             <br />
 Cyber Institute community members will join the best of the best, capable of contributing to and learning from a multi-dimensional space to define problems and seek solutions.
@@ -83,7 +83,7 @@ Cyber Institute community members will join the best of the best, capable of con
 We have three broad areas we wish to address; within each there are a range of smaller topics, often shorter-term, offering many opportunities for collaborations and partnerships.
             <br />
             <br />
-We aim to build a new system of knowledge to describe and shape the nature of the cyber world.  That means thinking long-term while understanding practice, behaviours and consequence in the here and now. It also means establishing better articulations of the effect of, for example, intrinsically broken systems and the effects those may have on governance, commerce, national security or the strategic calculus of war.  Doing so may assist decision-makers in building more resilient systems and structures.
+<span style={{'font-weight':'700', color:'#523178'}}>We aim to build a new system of knowledge to describe and shape the nature of the cyber world</span>.  That means thinking long-term while understanding practice, behaviours and consequence in the here and now. It also means establishing better articulations of the effect of, for example, intrinsically broken systems and the effects those may have on governance, commerce, national security or the strategic calculus of war.  Doing so may assist decision-makers in building more resilient systems and structures.
           </p>
         </div>
         <div className='col-md-5 margin4RemAuto-sm'>
@@ -100,10 +100,10 @@ We aim to build a new system of knowledge to describe and shape the nature of th
         </div>
         <div className='col-md-6'>
           <p>
-          We are interested in the future state of a world shaped by technology—and how that world exhibits resilience and management of the problems posed by cyber.  For example, what is the future of democracy and democratic norms in a world where the trust in core institutions no longer exist?  Or how do we assure the integrity of health systems in a world where individual genetic print may be bought and sold on the dark web?
+          We are interested in the future state of a world shaped by technology—and how that world exhibits <span style={{'font-weight':'700', color:'#523178'}}>resilience and management of the problems posed by cyber</span>.  For example, what is the future of democracy and democratic norms in a world where the trust in core institutions no longer exist?  Or how do we assure the integrity of health systems in a world where individual genetic print may be bought and sold on the dark web?
             <br />
             <br />
-We want to find ways to overcome or offset the current arms race of attack-defend-attack-defend in cyber security.  Such races typically are not won, but lost through exhaustion, loss of will or being overwhelmed.  Are there frameworks, different technologies or ways of doing that help individuals, organisations and nations withstand the dark side of digital technologies?
+<span style={{'font-weight':'700', color:'#523178'}}>We want to find ways to overcome or offset the current arms race of attack-defend-attack-defend in cyber security</span>.  Such races typically are not won, but lost through exhaustion, loss of will or being overwhelmed.  Are there frameworks, different technologies or ways of doing that help individuals, organisations and nations withstand the dark side of digital technologies?
           </p>
         </div>
       </div>
