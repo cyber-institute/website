6a9c4a430ee55291c1a73fb0c441b0544926439c
Joseph Tsindos <jtsindos@gmail.com>
2020-02-14 17:17:58 +1100

    Fix Footer on homepage.

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,34 +7,34 @@ class Footer extends Component {
   render () {
     return (
       <>
-        <style />
+        <div style={{ 'margin-top': '200px' }}>
+          <div className='footerContainer' />
 
-        <div className='footerContainer' style={{'margin-top':'200px'}} />
-
-        <div className='textContainer' style={{'margin-top':'200px'}}>
-          <div className='footerText'>
-            <p className='fontSize16px-xs'>
+          <div className='textContainer'>
+            <div className='footerText'>
+              <p className='fontSize16px-xs'>
         +61 2 6125 7802
-            </p>
-            <p>
-              <a className='fontSize16px-xs' href='mailto:cyber@anu.edu.au' style={{ color: '#D2ACF7', 'text-decoration': 'underline' }}>cyber@anu.edu.au</a>
-            </p>
-            <p className='fontSize16px-xs'>
+              </p>
+              <p>
+                <a className='fontSize16px-xs' href='mailto:cyber@anu.edu.au' style={{ color: '#D2ACF7', 'text-decoration': 'underline' }}>cyber@anu.edu.au</a>
+              </p>
+              <p className='fontSize16px-xs'>
         Level 2, 115 North Rd, Acton  ACT 2601
-            </p>
-            <p>
-              <a className='fontSize16px-xs' href='https://twitter.com/anucyber?lang=en' style={{ color: '#D2ACF7', 'text-decoration': 'underline' }}>twitter@AnuCyber</a>
-            </p>
+              </p>
+              <p>
+                <a className='fontSize16px-xs' href='https://twitter.com/anucyber?lang=en' style={{ color: '#D2ACF7', 'text-decoration': 'underline' }}>twitter@AnuCyber</a>
+              </p>
+            </div>
+
+            <img src={logo} className='logo' alt="The Cyber Institute's Logo in White" />
+
           </div>
 
-          <img src={logo} className='logo' alt="The Cyber Institute's Logo in White" />
-
-        </div>
-
-        <div className='purpleStrip fontSize16px-xs'>
-          <p>
+          <div className='purpleStrip fontSize16px-xs'>
+            <p>
     A STRATEGIC INITIATIVE OF THE AUSTRALIAN NATIONAL UNIVERSITY
-          </p>
+            </p>
+          </div>
         </div>
       </>
     )
