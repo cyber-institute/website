2fc542504dcac0d956e5f61e8453e99fc4263250
B00mhs99 <>
2020-02-14 14:34:04 +1100

    a

--- src/pages/Services/Articles.js
+++ src/pages/Services/Articles.js
@@ -4,6 +4,10 @@ import { DynamicPage } from '../../News/news'
 import SlideToggleContent from '../../ContentSlider/SlideToggleContent'
 import ViewMoreButton from '../../ContentSlider/ViewMoreButton'
 
+import SecurityControls from './../../../img/LaptoplockRAND.jpg'
+import Rethinking from './../../../img/realtimecyberattacks.jpg'
+import Broken from'./../../../img/menonboat.png'
+
 const news = {
   title: 'ReadingList',
   sections: [{
@@ -14,7 +18,7 @@ const news = {
       byline: 'In October 1967 a Task Force was organized by the Advanced Research Projects Agency (now the Defense Advanced Research Projects Agency) to study and recommend appropriate computer security safeguards that would protect classified information in multi-access, resource-sharing computer systems. Nearly a decade later the report is still a valuable comprehensive discussion of security controls for resource-sharing computer systems. Ideas first expressed in this report and even occasional figures from it have gradually seeped into the technical literature, but it still contains material that has not been published elsewhere.',
       author: 'Willis H. Ware',
       date: '1979',
-      imageSource: './../../img/LaptoplockRAND.jpg',
+      imageSource: {SecurityControls},
       sizeName: 'col-md-3 col-xs-12',
       link: 'https://www.rand.org/pubs/reports/R609-1.html'
     }
@@ -26,7 +30,7 @@ const news = {
       byline: 'It is time for government authorities - including law enforcement - to embrace encryption because it is one of the few mechanisms that the United States and its allies can use to more effectively protect themselves from existential cybersecurity threats. ',
       author: 'Jim Baker',
       date: '22/10/2019',
-      imageSource: './../../img/realtimecyberattacks.jpg',
+      imageSource: {Rethinking},
       sizeName: 'col-md-3 col-xs-12',
       link: 'https://www.lawfareblog.com/rethinking-encryption'
     }
@@ -38,7 +42,7 @@ const news = {
       byline: 'Computers, and computing, are broken. It’s hard to explain to regular people how much technology barely works, how much the infrastructure of our lives is held together by the IT equivalent of baling wire',
       author: 'Quinn Norton',
       date: '20/05/2014',
-      imageSource: './img/ai1.jpg',
+      imageSource: {Broken},
       sizeName: 'col-md-3 col-xs-12',
       link: 'https://medium.com/message/everything-is-broken-81e5f33a24e1'
     }
