abd625f74a50fe488ed610247be2ec068d97f31f
B00mhs99 <>
2020-06-05 11:28:34 +1000

    Tried to add link

--- src/pages/Privacy/WhatAreWeDoing.js
+++ src/pages/Privacy/WhatAreWeDoing.js
@@ -139,13 +139,14 @@ export default ({ image1, image2, image3 }) =>
             <p>Introducing SafeLy. This plugin lets you use different identities for different accounts, protecting your privacy and your security online.
             </p>
             <br />
-            <p style={{color:"#523178", fontWeight:"700"}}> SafeLy is currently pending approval from the Chrome web store. Watch this space for it's impending release!</p>
+            <p style={{color:"#523178", fontWeight:"700"}}> SafeLy is now available for download from the Chrome web store.</p>
             <br />
             <div style={{display:"flex", justifyContent:"center"}}>
               <form>
-                <button  style={{backgroundColor:"#DDDDDD", border:"none", color:"white",
+              <a target="_blank" href='https://chrome.google.com/webstore/detail/safely/epbbhpoagmgdnfnckmjdhlaknncohibd'>
+                <button  style={{backgroundColor:"#523178", border:"none", color:"white",
             padding:"15px 25px", textAlign:"center", cursor:"pointer", borderRadius:"25px",
-            fontSize:"18px", fontWeight:"100"}} onclick="https://cyber.anu.edu.au/">Download SafeLy for Chrome</button>
+            fontSize:"18px", fontWeight:"100"}} >Download SafeLy for Chrome</button></a>
               </form>
               
             </div>
