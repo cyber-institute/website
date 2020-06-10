ea6206d9487e4e0b9806ed285bac2177d9147b42
B00mhs99 <>
2020-06-10 16:06:06 +1000

    Added snackmenu

---
+++ src/Body/SnackMenu.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function CustomizedSnackbars() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+        setOpen(false);
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Snackbar open={open} autoHideDuration={6000} >
+        <Alert onClose={handleClose} style={{"background":"#523178"}}>
+        <a href="https://www.anu.edu.au/giving/support-us/advancing-our-nation/the-cyber-institute-general-fund" style={{"color":"white"}}> Want to support the Cyber Institute? Give us a gift!
+        </a>
+        </Alert>
+        
+      </Snackbar>
+    </div>
+  );
+}
